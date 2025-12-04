822880fd4186e2cd4f2d3a7bce9742e9e6883d9c
Vania S. <vanjasavsjuk@gmail.com>
2025-12-04 16:05:08 +0000

    [{ 	"resource": "/home/user/studio/src/components/icons/sirius-logo.tsx"

--- src/components/icons/sirius-logo.tsx
+++ src/components/icons/sirius-logo.tsx
@@ -1,8 +1,8 @@
-import type { SVGProps } from "react";
+import type { SVGProps } from 'react';
 import Image from 'next/image';
 
-export function SiriusLogo(props: SVGProps<SVGSVGElement> & {width?: number; height?: number}) {
+export function SiriusLogo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
-    <Image src="/siriuslogo.svg" alt="Sirius Logo" width={props.width || 24} height={props.height || 24} {...props} />
+    <Image src="/siriuslogo.svg" alt="Sirius Logo" width={24} height={24} {...props} />
   );
 }
