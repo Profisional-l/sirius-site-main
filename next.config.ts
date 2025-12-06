76f87603576558371069c5e5ce05f68d0092d660
Jake Sooly <96531067+Profisional-l@users.noreply.github.com>
2025-12-06 20:03:44 +0300

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
@@ -29,9 +25,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-   experimental: {
-    serverComponentsExternalPackages: ['framer-motion'],
   },
 };
 
